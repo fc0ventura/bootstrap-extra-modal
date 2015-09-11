ff7bb98de45abb1f1f59b1bcd964ca4d86e883ee
Vasco Gaspar <vvascogasparr@gmail.com>
2015-09-11 16:12:43 +0100

    Added grunt-watch and grunt-postcss + precss

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,13 +3,45 @@ module.exports = function(grunt) {
   // Project configuration.
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
+
+    watch: {
+      scripts: {
+        files: ['src/js/<%= pkg.name %>.js'],
+        tasks: ['uglify'],
+        options: {
+          spawn: false,
+        },
+      },
+
+      css: {
+        files: ['src/css/*.css'],
+        tasks: ['postcss']
+      }
+    },
     uglify: {
       options: {
-        banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
+        banner: '/*! <%= pkg.name %> by Vasco Gaspar */\n/*! Last Updated: <%= grunt.template.today("yyyy-mm-dd") %> */\n'
       },
       build: {
-        src: '<%= pkg.name %>.js',
-        dest: 'dist/<%= pkg.name %>.min.js'
+        src: 'src/js/<%= pkg.name %>.js',
+        dest: 'dist/js/<%= pkg.name %>.min.js'
+      }
+    },
+
+    postcss: {
+      options: {
+        map: true, // inline sourcemaps
+        parser: require('postcss-scss'),
+        processors: [
+          require('pixrem')(), // add fallbacks for rem units
+          require('autoprefixer')({browsers: 'last 2 versions'}), // add vendor prefixes
+          require('cssnano')(), // minify the result
+          require('precss')() // SASS like syntax
+        ]
+      },
+      dist: {
+        src: 'src/css/*.css',
+        dest: 'dist/css/<%= pkg.name %>.css'
       }
     }
   });
@@ -17,7 +49,13 @@ module.exports = function(grunt) {
   // Load the plugin that provides the "uglify" task.
   grunt.loadNpmTasks('grunt-contrib-uglify');
 
+  // PostCSS Task
+  grunt.loadNpmTasks('grunt-postcss');
+
+  // Whatch for file changes and run default tasks
+  grunt.loadNpmTasks('grunt-contrib-watch');
+
   // Default task(s).
-  grunt.registerTask('default', ['uglify']);
+  grunt.registerTask('default', ['uglify'], ['postcss']);
 
 };
